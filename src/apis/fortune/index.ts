e522c0d983b61ef048c8c5b8a68a29fe0345a38c
zzangjyj0818 <jyj010818@naver.com>
2025-09-16 08:41:14 +0900

    [#12] Feat: Fortune 관련 API 인스턴스 구현

---
+++ src/apis/fortune/index.ts
@@ -0,0 +1,22 @@
+import ApiBuilder from '../config/builder/ApiBuilder';
+import type { FortuneResponse, FortuneCreateRequest } from './index.type';
+
+const createFortune = (description: string) => {
+  return ApiBuilder.create<FortuneCreateRequest, number>('/api/v1/fortunes')
+    .setMethod('POST')
+    .setData({ description });
+};
+
+const assignFortune = () => {
+  return ApiBuilder.create<void, FortuneResponse>(
+    '/api/v1/fortunes/assign',
+  ).setMethod('POST');
+};
+
+const getMyFortunes = () => {
+  return ApiBuilder.create<void, FortuneResponse[]>(
+    '/api/v1/fortunes',
+  ).setMethod('GET');
+};
+
+export { createFortune, assignFortune, getMyFortunes };
