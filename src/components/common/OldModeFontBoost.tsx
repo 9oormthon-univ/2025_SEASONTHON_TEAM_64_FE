c8646d64bb3f99b469a07e84cfff6918cd7a1855
zzangjyj0818 <jyj010818@naver.com>
2025-09-18 08:46:08 +0900

    [#34] Fix: 로그인 무한 새로고침 오류 해결

--- src/components/common/OldModeFontBoost.tsx
+++ src/components/common/OldModeFontBoost.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { useApiQuery } from '../../apis/config/builder/ApiBuilder';
 import { getMemberDetail } from '../../apis/member';
+import { hasToken } from '../../utils/authState';
 
 const OldModeFontBoost: React.FC = () => {
-  const { data } = useApiQuery(getMemberDetail(), ['member']);
+  // 로그인/온보딩 화면에서는 토큰이 없어야 정상이며, 이때 쿼리를 막아 401 -> /login 무한 리다이렉트 방지
+  const authed = hasToken();
+  const { data } = useApiQuery(getMemberDetail(), ['member'], {
+    queryKey: ['member'],
+    enabled: authed,
+  });
 
   React.useEffect(() => {
     const root = document.documentElement;
